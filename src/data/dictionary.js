816270233df214f40e690d5f9d60962d12f78f48
4k1r1n <arinakire@gmail.com>
2023-08-20 13:04:24 +0300

    refactor: format dictionary

--- src/data/dictionary.js
+++ src/data/dictionary.js
@@ -1,61 +1,61 @@
-const translation = {
-    "ru": {
-        greeting: ["Доброе утро", "Добрый день", "Добрый вечер", "Доброй ночи"],
-        placeholderName: "[Введите имя]",
-        defaultCity: "минск",
-        placeholderCity: "Введите город",
-        windSpeed: "Скорость ветра",
-        windSpeedUnit: "m/s",
-        weatherHumidity: "Влажность",
-        weatherError: "Ошибка: город не найден (404) :-(",
-        settingsTitle: "Настройки",
-        changeBtn: "Изменить",
-        language: "Русский",
+const dictionary = {
+  ru: {
+    greeting: ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Доброй ночи'],
+    placeholderName: '[Введите имя]',
+    defaultCity: 'минск',
+    placeholderCity: 'Введите город',
+    windSpeed: 'Скорость ветра',
+    windSpeedUnit: 'm/s',
+    weatherHumidity: 'Влажность',
+    weatherError: 'Ошибка: город не найден (404) :-(',
+    settingsTitle: 'Настройки',
+    changeBtn: 'Изменить',
+    language: 'Русский',
 
-        settingNames: {
-            langSetting: "Язык (англ/рус)",
-            photoSource: "Источник фоновых изображений",
-            widgets: "Показать виджеты",
-        },
-
-        widgetsNames: {
-            widgetTime: "Время",
-            widgetDate: "Дата",
-            widgetGreeting: "Приветствие",
-            widgetQuote: "Цитата дня",
-            widgetWeather: "Погода",
-            widgetAudioPlayer: "Аудиоплеер",
-        }
+    settingNames: {
+      langSetting: 'Язык (англ/рус)',
+      photoSource: 'Источник фоновых изображений',
+      widgets: 'Показать виджеты',
     },
-    "en": {
-        greeting: ["Good morning", "Good afternoon", "Good evening", "Good night"],
-        timeOfDay: ["morning", "afternoon", "evening", "night"],
-        placeholderName: "[Enter name]",
-        defaultCity: "minsk",
-        placeholderCity: "Enter city",
-        windSpeed: "Wind speed",
-        windSpeedUnit: "м/с",
-        weatherHumidity: "Humidity",
-        weatherError: "Error: Not found city (404) :-(",
-        settingsTitle: "Settings",
-        changeBtn: "Change",
-        language: "English",
 
-        settingNames: {
-            langSetting: "Language (en/ru)",
-            photoSource: "Background images source",
-            widgets: "Show widgets",
-        },
+    widgetsNames: {
+      widgetTime: 'Время',
+      widgetDate: 'Дата',
+      widgetGreeting: 'Приветствие',
+      widgetQuote: 'Цитата дня',
+      widgetWeather: 'Погода',
+      widgetAudioPlayer: 'Аудиоплеер',
+    },
+  },
+  en: {
+    greeting: ['Good morning', 'Good afternoon', 'Good evening', 'Good night'],
+    timeOfDay: ['morning', 'afternoon', 'evening', 'night'],
+    placeholderName: '[Enter name]',
+    defaultCity: 'minsk',
+    placeholderCity: 'Enter city',
+    windSpeed: 'Wind speed',
+    windSpeedUnit: 'м/с',
+    weatherHumidity: 'Humidity',
+    weatherError: 'Error: Not found city (404) :-(',
+    settingsTitle: 'Settings',
+    changeBtn: 'Change',
+    language: 'English',
 
-        widgetsNames: {
-            widgetTime: "Time",
-            widgetDate: "Date",
-            widgetGreeting: "Greeting",
-            widgetQuote: "Quote of the day",
-            widgetWeather: "Weather forecast",
-            widgetAudioPlayer: "Audio player",
-        }
-    }
-}
+    settingNames: {
+      langSetting: 'Language (en/ru)',
+      photoSource: 'Background images source',
+      widgets: 'Show widgets',
+    },
 
-export default translation;
+    widgetsNames: {
+      widgetTime: 'Time',
+      widgetDate: 'Date',
+      widgetGreeting: 'Greeting',
+      widgetQuote: 'Quote of the day',
+      widgetWeather: 'Weather forecast',
+      widgetAudioPlayer: 'Audio player',
+    },
+  },
+};
+
+export default dictionary;
