26478e5a7031c7858d28d811738550e9843991e2
4k1r1n <arinakire@gmail.com>
2023-09-01 16:47:47 +0300

    refactor: rename playlist file

---
+++ src/data/playlist.js
@@ -0,0 +1,33 @@
+import downtownGlowSrc from '../assets/sounds/Downtown-Glow.mp3';
+import luminanceSrc from '../assets/sounds/Luminance.mp3';
+import soonFlySrc from '../assets/sounds/Soon-Well-Fly.mp3';
+import stormCloudsSrc from '../assets/sounds/Storm-Clouds.mp3';
+
+const playlist = [
+  {
+    title: 'Downtown Glow',
+    artist: 'Lo-fi',
+    src: downtownGlowSrc,
+    duration: '02:52',
+  },
+  {
+    title: 'Luminance',
+    artist: 'Lo-fi',
+    src: luminanceSrc,
+    duration: '03:12',
+  },
+  {
+    title: "Soon We'll Fly",
+    artist: 'Lo-fi',
+    src: soonFlySrc,
+    duration: '04:24',
+  },
+  {
+    title: 'Storm Clouds',
+    artist: 'Lo-fi',
+    src: stormCloudsSrc,
+    duration: '02:36',
+  },
+];
+
+export default playlist;
