fe4d260518757339c20f052948f92b1104fc326c
4k1r1n <arinakire@gmail.com>
2023-09-25 01:11:26 +0300

    feat: update weather widget

--- src/components/widgets/weather/weather.ts
+++ src/components/widgets/weather/weather.ts
@@ -5,7 +5,6 @@ import State from '../../../interfaces/state';
 import { getLanguage } from '../../../state';
 import { OpenWeatherMapData } from '../../../types/api-data';
 import createNode from '../../../utils/create-node';
-import WeatherElements from '../../../types/weather';
 
 // eslint-disable-next-line prettier/prettier
 const getWeatherUrl = (lang: State['language'], city: string): string => `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric`;
@@ -24,33 +23,73 @@ async function getWeatherData(
   }
 }
 
+const renderError = (language: State['language']): HTMLElement => {
+  const error = createNode('p', ['weather__error']);
+  error.textContent = `${dictionary[language].weatherError}`;
+  return error;
+};
+
+const renderInfo = (data: OpenWeatherMapData, language: State['language']): DocumentFragment => {
+  const fragment = new DocumentFragment();
+  const icon = createNode('i', ['weather__icon', 'owf']);
+  const temperature = createNode('p', ['weather__temperature']);
+  const description = createNode('p', ['weather__description']);
+  const wind = createNode('p', ['weather__wind']);
+  const humidity = createNode('p', ['weather__humidity']);
+  const { windSpeedUnit } = dictionary[language];
+  icon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${Math.round(data.main.temp)}°C`;
+  wind.textContent = `${dictionary[language].windSpeed}: ${Math.round(
+    data.wind.speed,
+  )} ${windSpeedUnit}`;
+  humidity.textContent = `${dictionary[language].weatherHumidity}: ${Math.round(
+    data.main.humidity,
+  )}%`;
+  description.textContent = data.weather[0].description;
+  fragment.append(icon, temperature, description, wind, humidity);
+  return fragment;
+};
+
+const addAnimation = (element: HTMLElement): void => {
+  element.classList.add('fade-in');
+  element.addEventListener(
+    'animationend',
+    () => {
+      element.classList.remove('fade-in');
+    },
+    { once: true },
+  );
+};
+
 const getWeather = async (
-  lang: State['language'],
-  city: string,
-  elements: WeatherElements,
+  language: State['language'],
+  value: string,
+  infoElement: HTMLElement,
 ): Promise<void> => {
-  const { icon, temperature, description, wind, humidity, error } = elements;
-  const data = await getWeatherData(lang, city);
-  const thirdClassName = icon.classList.item(2);
+  const data = await getWeatherData(language, value);
   try {
-    icon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${Math.round(data.main.temp)}°C`;
-    // eslint-disable-next-line prettier/prettier
-    wind.textContent = `${dictionary[lang].windSpeed}: ${Math.round(data.wind.speed)} ${dictionary[lang].windSpeedUnit}`;
-    humidity.textContent = `${dictionary[lang].weatherHumidity}: ${Math.round(
-      data.main.humidity,
-    )}%`;
-    description.textContent = data.weather[0].description;
-    error.textContent = '';
+    const info = renderInfo(data, language);
+    addAnimation(infoElement);
+    infoElement.replaceChildren(info);
   } catch {
-    if (thirdClassName) {
-      icon.classList.remove(thirdClassName);
-    }
-    error.textContent = `${dictionary[lang].weatherError}`;
-    wind.textContent = '';
-    temperature.textContent = '';
-    humidity.textContent = '';
-    description.textContent = '';
+    const error = renderError(language);
+    addAnimation(infoElement);
+    infoElement.replaceChildren(error);
+  }
+};
+
+const handleInputChange = (
+  weatherElement: HTMLElement,
+  infoElement: HTMLElement,
+  inputElement: HTMLElement,
+  language: State['language'],
+): void => {
+  if (!infoElement.parentElement) {
+    weatherElement.append(infoElement);
+  }
+  if (inputElement instanceof HTMLInputElement) {
+    const city = inputElement.value;
+    getWeather(language, city, infoElement);
   }
 };
 
@@ -58,22 +97,12 @@ const renderWeather = (): HTMLElement => {
   const language = getLanguage();
   const weather = createNode('div', ['header__weather', 'weather']);
   const input = createNode('input', ['weather__city', 'input']);
+  const info = createNode('div', ['weather__info']);
   input.setAttribute('type', 'text');
   input.setAttribute('placeholder', `${dictionary[language].placeholderCity}`);
-  const icon = createNode('i', ['weather__icon', 'owf']);
-  const info = createNode('div', ['weather__info']);
-  const temperature = createNode('p', ['weather__temperature']);
-  const description = createNode('p', ['weather__description']);
-  const wind = createNode('p', ['weather__wind']);
-  const humidity = createNode('p', ['weather__humidity']);
-  const error = createNode('p', ['weather__error']);
-  const elements = { icon, temperature, description, wind, humidity, error };
-  info.append(temperature, description);
-  weather.append(input, icon, info, wind, humidity, error);
+  weather.append(input);
   input.addEventListener('change', () => {
-    if (input instanceof HTMLInputElement) {
-      getWeather(language, input.value, elements);
-    }
+    handleInputChange(weather, info, input, language);
   });
   return weather;
 };
